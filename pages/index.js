4adcd52bfbf2e401f93543ae53952dca66b8c28e
Mikheull <mikhael.bailly1@orange.fr>
2021-01-15 02:44:46 +0100

    add geolocate and geocoder controls

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,22 @@ export default function Home() {
         center: [lng, lat],
         zoom: zoom,
       });
+
+      map.addControl(
+        new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true,
+          },
+          trackUserLocation: true,
+        })
+      );
+      
+      map.addControl(
+        new MapboxGeocoder({
+          accessToken: mapboxgl.accessToken,
+          mapboxgl: mapboxgl
+        })
+      );
     }
   }, [pageIsMounted]);
 
@@ -33,6 +49,10 @@ export default function Home() {
         <title>Velib</title>
         <link rel="icon" href="/favicon.ico" />
         <link href='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css' rel='stylesheet' />
+        <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.min.js"></script>
+        <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.css" type="text/css"/>
+        <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script>
       </Head>
 
       <div id="map-container" style={{ height: "100vh", width: "100vw" }} />
