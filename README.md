970c8db14f6fe9e05e25952ad0dbef594080b715
Mikheull <mikhael.bailly1@orange.fr>
2021-01-17 23:13:42 +0100

    Add the Meteo (on Paris) and remove unused files

--- README.md
+++ README.md
@@ -6,3 +6,34 @@ This is a starter template for [Learn Next.js](https://nextjs.org/learn).
 
 ## Dates :
 *Start at Thursday 15 January 2021*
+
+
+## Todos :
+- [ ] **Map**
+- [x] Afficher une map centrée sur Paris
+- [ ] Afficher les stations de vélib
+- [x] Demander la localisation
+- [ ] Si localisation = afficher station proche de vous
+- [ ] Popin d'informations d'une station (avec l'affluence)
+- [ ] Bouton pour faire un trajet d'une station vers mon adresse
+- [ ] Trier les stations (mecanique, electrique, places)
+- [x] Rechercher une adresse
+- [ ] Faire des markers cluster
+- [ ] Analyser les taux de stations des 4 dernières semaines
+- [ ] Afficher les prédictions d'une station sur 1 semaine
+<br>
+
+- [ ] **Help**
+- [ ] Pages d'aide avec navigations/slider
+<br>
+
+- [ ] **Terms**
+- [ ] CGU
+- [ ] RGPD
+<br>
+
+- [ ] **Météo**
+- [ ] Demander la localisation
+- [ ] Récupérer la météo de la position
+- [ ] Afficher les icons de météo correspondante
+<br>
--- pages/meteo.js
+++ pages/meteo.js
@@ -1,12 +1,15 @@
 import Head from 'next/head'
 import axios from 'axios'
+import moment from 'moment';
 
 import { useRef, useEffect, useState } from "react";
 import '../public/styles/meteo.module.css'
+import 'moment/locale/fr';
 
 export default function Home() {
   const [pageIsMounted, setPageIsMounted] = useState(false);
- 
+  const [temp, setTemp] = useState(Number)
+  const [icon, setIcon] = useState(String)
 
   useEffect(() => {
     setPageIsMounted(true);
@@ -14,28 +17,41 @@ export default function Home() {
 
   useEffect(() => {
     if (pageIsMounted) {
-        // Paris to default
 
-        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${process.env.METEO_ACCESS_TOKEN}&lang=fr&units=metric`)
-          .then(function (response) {
-            console.log(response);
-          }).catch(err => {
-              console.log(err);
-          });
+      axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${process.env.METEO_ACCESS_TOKEN}&lang=fr&units=metric`)
+        .then(function (response) {
+          // console.log(response);
+
+          setTemp(response.data.main.temp)
+          setIcon(response.data.weather[0].main)
+          
+        }).catch(err => {
+            console.log(err);
+        });
     }
   }, [pageIsMounted]);
 
   return (
-    <div className="meteo_container">
+    <div className="container">
 
       <Head>
         <title>Météo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>
-          Météo
+      <div className="meteo_container">
+
+        <div className="meteo">
+          <a href="../" className="back_button"><img src="images/icons/next.svg"></img></a>
+          <div className="header">
+            <div className="temp">{temp} °C</div>
+            <div className="icon"><img src="images/icons/Clear.svg"></img></div>
+          </div>
+          <div className="date">{moment().format('DD/MM/YY')}</div>
+          <div className="day">{moment().format('dddd')}</div>
+        </div>
       </div>
+
     </div>
   )
 }
