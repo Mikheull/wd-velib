ce832d32ad08fb4ccc22f99448b8661c0c7af5bf
Mikheull <mikhael.bailly1@orange.fr>
2021-01-18 23:26:36 +0100

    updare Map styles

---
+++ components/StationDetails.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+
+// Styles
+import '../public/styles/map.module.css';
+
+class StationDetails extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let items = [];
+        for (let i = 0; i <= 23; i++) {
+            items.push(<div key={i} className="item_timeline">
+                <p>{i}:00</p>
+                <div>
+                    <div className="bar_mechanical bar"></div>
+                    <div className="bar_electric bar"></div>
+                    <div className="bar_place bar"></div>
+                </div>
+            </div>)
+          }
+          
+        return (
+            <section className={"station_details " + (this.props.visible ? 'show' : 'hidden')}>
+                <div className="header">
+                    <span className="close" onClick={this.props.closeMethod}><img src="images/icons/close.svg" alt=""/></span>
+                    <h2 className="title">{this.props.data.name}</h2>
+                    <div className="affluence">
+                        <span><img src="images/icons/affluence_1.svg" alt=""/></span>
+                        <span><img src="images/icons/affluence_1.svg" alt=""/></span>
+                        <span><img src="images/icons/affluence_1.svg" alt=""/></span>
+                        <span><img src="images/icons/affluence_1.svg" alt=""/></span>
+                        <span><img src="images/icons/affluence_0.svg" alt=""/></span>
+                        <p>Très affluente</p>
+                    </div>
+                </div>
+                
+                <div className="body">
+                    <div className="wrapper">
+                        <div className="title">Description <span className="code">#{this.props.data.stationcode}</span></div>
+                        <p className="desc">Cette station situé à {this.props.data.nom_arrondissement_communes} peut contenir jusqu'à {this.props.data.capacity} vélos. La station est actuellement {(this.props.data.is_installed == 'OUI' && this.props.data.is_returning == 'OUI') ? 'ouverte' : 'fermée'}. Vous {(this.props.data.is_renting == 'OUI') ? 'pouvez' : 'ne pouvez pas'} prendre un abonnement directement sur place via la borne !</p>
+                        
+                        <div className="separator"></div>
+                    
+                        <div className="details">
+                            <div className="item_details mechanical">
+                                <p>{this.props.data.mechanical}</p>
+                                <img src="images/icons/mechanical.svg" alt=""/>
+                            </div>
+                            <div className="item_details electric">
+                                <p>{this.props.data.ebike}</p>
+                                <img src="images/icons/electric.svg" alt=""/>
+                            </div>
+                            <div className="item_details place">
+                                <p>{this.props.data.numdocksavailable}</p>
+                                <img src="images/icons/place.svg" alt=""/>
+                            </div>
+                        </div>
+                        <button className="button">Y aller</button>
+                        <div className="separator"></div>
+                    </div>
+                    
+                    <div className="wrapper_prediction">
+                        <div className="title">Quand avoir un vélo ?</div>
+                    
+                        <div className="timeline">{items}</div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default StationDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,219 +1,226 @@
-import Head from 'next/head'
-import { useRef, useEffect, useState } from "react";
+import React, {Component, useRef, useEffect, useState} from 'react';
 import axios from 'axios'
-
-import stations from '../public/data/stations.json';
-import '../public/styles/map.module.css'
-
 const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
 
-export default function Home() {
-  const [pageIsMounted, setPageIsMounted] = useState(false);
-  const [lng, setLng] = useState(2.3797426);
-  const [lat, setLat] = useState(48.8960154);
-  const [zoom, setZoom] = useState(11);
+// Components
+import Head from 'next/head'
+import StationDetails from '../components/StationDetails';
 
-  useEffect(() => {
-    setPageIsMounted(true);
-  }, []);
+// Styles
+import '../public/styles/map.module.css';
 
-  useEffect(() => {
-    if (pageIsMounted) {
-      mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN;
+// Datas
+import stations from '../public/data/stations.json';
 
-      // Init
-      const map = new mapboxgl.Map({
-        container: "map-container",
-        style: "mapbox://styles/mapbox/light-v10",
-        center: [lng, lat],
-        zoom: zoom,
-      });
+class Home extends Component {
+  _isMounted = false;
 
-      // Geolocalisation
-      map.addControl(
-        new mapboxgl.GeolocateControl({
-          positionOptions: {
-            enableHighAccuracy: true,
-          },
-          trackUserLocation: true,
-        })
-      );
-      
-      // Search input
-      map.addControl(
-        new MapboxGeocoder({
-          accessToken: mapboxgl.accessToken,
-          mapboxgl: mapboxgl
-        })
-      );
+  constructor(props) {
+    super(props);
 
-      // Load stations
-      map.on('load', async function () {
-       
-        // Ajout du fichier source des stations
-        map.addSource('points', {
-          'type': 'geojson',
-          'data': {
-            'type': 'FeatureCollection',
-            'features': stations
-          },
-          'cluster': true,
-          'clusterMaxZoom': 12,
-          'clusterRadius': 50,
-          'clusterProperties': {
-            'sum': ["+", 1],
-          },
-        });
-        
-        // Add a symbol layer
-        map.addLayer({
-          id: "clusters",
-          type: "circle",
-          source: "points",
-          filter: ["has", "point_count"],
-          paint: {
-            "circle-color": "rgb(229, 36, 59)",
-            "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
-            "circle-opacity": 0.75,
-            "circle-stroke-width": 4,
-            "circle-stroke-color": "#fff",
-            "circle-stroke-opacity": 0.5,
-          },
-        });
-      
-        map.addLayer({
-          id: "cluster-count",
-          type: "symbol",
-          source: "points",
-          filter: ["has", "point_count"],
-          layout: {
-            "text-field": "{sum}",
-            "text-font": ["Open Sans Bold"],
-            "text-size": 16,
-          },
-          paint: {
-            "text-color": "white",
-          },
-        });
-      
-        map.addLayer({
-          id: "unclustered-point",
-          type: "circle",
-          source: "points",
-          filter: ["!", ["has", "point_count"]],
-          paint: {
-            "circle-radius": ["step", ["get", "event_count"], 20, 100, 30, 750, 40],
-            "circle-color": "rgb(229, 36, 59)",
-            "circle-opacity": 0.75,
-            "circle-stroke-width": 4,
-            "circle-stroke-color": "#fff",
-            "circle-stroke-opacity": 0.5,
-          },
-        });
-
-        map.addLayer({
-          id: "unclustered-point-label",
-          type: "symbol",
-          source: "points",
-          minzoom: 15,
-          layout: {
-              "text-field": "{name}",
-              "text-anchor": "top",
-              "text-offset": [0,0.5],
-          },
-          paint: {
-            "text-color": "rgb(229, 36, 59)"
-          }
-        });
-      
-
-        map.on("mouseenter", "unclustered-point", function () {
-          map.getCanvas().style.cursor = "pointer";
-        });
-        map.on("mouseleave", "unclustered-point", function () {
-          map.getCanvas().style.cursor = "";
-        });
-
-        // Popin de station
-        map.on("click", "unclustered-point", function (e) {
-          var coordinates = e.features[0].geometry.coordinates.slice();
-          while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-          }
-          
-          let popin = 'Une erreur est survenue';
-          // Retour API et création du template de la popin
-          axios.get(`https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=stationcode%3D${e.features[0].properties.stationcode}`)
-          .then(function (response) {
-              let _data = response.data.records;
-              const pre = _data[0].fields;
-
-              popin = `
-                <div class="station_details">
-                  <div class="header">
-                    <h2 class="title">${pre.name}</h2>
-                    <span class="subtitle">${pre.stationcode}</span>
-                  </div>
-
-                  <p class="desc">Cette station situé à ${pre.nom_arrondissement_communes} peut contenir jusqu'à ${pre.capacity} vélos. La station est actuellement ${(pre.is_installed == 'OUI' && pre.is_returning == 'OUI') ? 'ouverte' : 'fermée'}. Vous ${(pre.is_renting == 'OUI') ? 'pouvez' : 'ne pouvez pas'} prendre un abonnement directement sur place via la borne !<p>
-                  
-                  <div class="separator"></div>
-                  
-                  <div class="details">
-                    <div class="item_details mechanical">
-                      <p>${pre.mechanical}</p>
-                      <img src="images/icons/mechanical.svg" alt=""/>
-                    </div>
-                    <div class="item_details electric">
-                      <p>${pre.ebike}</p>
-                      <img src="images/icons/electric.svg" alt=""/>
-                    </div>
-                    <div class="item_details place">
-                      <p>${pre.numdocksavailable}</p>
-                      <img src="images/icons/place.svg" alt=""/>
-                    </div>
-                  </div>
-                </div>
-              `;
-              new mapboxgl.Popup()
-                .setLngLat(coordinates)
-                .setHTML(popin)
-                .addTo(map);
-
-          }).catch(err => {
-              console.log(err);
-              new mapboxgl.Popup()
-                .setLngLat(coordinates)
-                .setHTML(popin)
-                .addTo(map);
-          });
-          
-        });
-
-        // Zoom on cluster
-        map.on("click", "clusters", function (e) {
-          map.flyTo({center: e.features[0].geometry.coordinates, zoom: map.getZoom() + 1 });
-        });
-
-      });
-        
+    this.state = {
+      lng: 2.3797426,
+      lat: 48.8960154,
+      zoom: 11,
+      stationsData: {
+        name: null, 
+        stationcode: null, 
+        nom_arrondissement_communes: null, 
+        capacity: null, 
+        is_installed: null, 
+        is_returning: null, 
+        is_renting: null, 
+        mechanical: null, 
+        ebike: null, 
+        numdocksavailable: null
+      },
+      stationVisibility: false
     }
-  }, [pageIsMounted]);
+  }
 
-  return (
-    <div className="container">
+  static async getInitialProps({res}) {
+    try {
+      return true;
+    } catch (e) {
+      res.statusCode = e;
+      return {statusCode: e};
+    }
+  }
+
+    
+  componentDidMount() {
+    var self = this;
+    mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN;
+
+    // Init
+    const map = new mapboxgl.Map({
+      container: "map-container",
+      style: "mapbox://styles/mapbox/light-v10",
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom,
+    });
+
+    // Geolocalisation
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+      })
+    );
+    
+    // Search input
+    map.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+      })
+    );
+
+
+    // Load stations
+    map.on('load', async function () {
       
-      <Head>
-        <title>Velib x Waze</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link href='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css' rel='stylesheet' />
-        <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.min.js"></script>
-        <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.css" type="text/css"/>
-        <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script>
-      </Head>
+      // Ajout du fichier source des stations
+      map.addSource('points', {
+        'type': 'geojson',
+        'data': {
+          'type': 'FeatureCollection',
+          'features': stations
+        },
+        'cluster': true,
+        'clusterMaxZoom': 12,
+        'clusterRadius': 50,
+        'clusterProperties': {
+          'sum': ["+", 1],
+        },
+      });
+      
+      // Add a symbol layer
+      map.addLayer({
+        id: "clusters",
+        type: "circle",
+        source: "points",
+        filter: ["has", "point_count"],
+        paint: {
+          "circle-color": "rgb(229, 36, 59)",
+          "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
+          "circle-opacity": 0.75,
+          "circle-stroke-width": 4,
+          "circle-stroke-color": "#fff",
+          "circle-stroke-opacity": 0.5,
+        },
+      });
+    
+      map.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "points",
+        filter: ["has", "point_count"],
+        layout: {
+          "text-field": "{sum}",
+          "text-font": ["Open Sans Bold"],
+          "text-size": 16,
+        },
+        paint: {
+          "text-color": "white",
+        },
+      });
+    
+      map.addLayer({
+        id: "unclustered-point",
+        type: "circle",
+        source: "points",
+        filter: ["!", ["has", "point_count"]],
+        paint: {
+          "circle-radius": ["step", ["get", "event_count"], 20, 100, 30, 750, 40],
+          "circle-color": "rgb(229, 36, 59)",
+          "circle-opacity": 0.75,
+          "circle-stroke-width": 4,
+          "circle-stroke-color": "#fff",
+          "circle-stroke-opacity": 0.5,
+        },
+      });
 
-      <div id="map-container"/>
-    </div>
-  )
+      map.addLayer({
+        id: "unclustered-point-label",
+        type: "symbol",
+        source: "points",
+        minzoom: 15,
+        layout: {
+            "text-field": "{name}",
+            "text-anchor": "top",
+            "text-offset": [0,0.5],
+        },
+        paint: {
+          "text-color": "rgb(229, 36, 59)"
+        }
+      });
+    
+
+      map.on("mouseenter", "unclustered-point", function () {
+        map.getCanvas().style.cursor = "pointer";
+      });
+      map.on("mouseleave", "unclustered-point", function () {
+        map.getCanvas().style.cursor = "";
+      });
+
+      // Popin de station
+      map.on("click", "unclustered-point", function (e) {
+        // Retour API et affichage de la popin
+        axios.get(`https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=stationcode%3D${e.features[0].properties.stationcode}`)
+          .then((response) => {
+            let _data = response.data.records;
+            const pre = _data[0].fields;
+            self.setState({stationsData: pre});
+            self.setState({stationVisibility: true});
+          })
+        .catch((error)=>{
+          console.log(error);
+        });
+      });
+
+      // Zoom on cluster
+      map.on("click", "clusters", function (e) {
+        map.flyTo({center: e.features[0].geometry.coordinates, zoom: map.getZoom() + 1 });
+      });
+
+    });
+  }
+
+  componentWillUnmount() {
+    
+  }
+
+  closeStation() {
+    this.setState({stationVisibility: false});
+  }
+
+  render() {
+      return (
+        <div className="container">
+        
+          <Head>
+            <title>Velib x Waze</title>
+            <link rel="icon" href="/favicon.ico" />
+            <link href='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css' rel='stylesheet' />
+            <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.min.js"></script>
+            <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.css" type="text/css"/>
+            <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script>
+          </Head>
+    
+          <div id="map-container"/>
+          <StationDetails 
+            key="x" 
+            data={this.state.stationsData} 
+            visible={this.state.stationVisibility}
+            closeMethod={() => this.closeStation()} 
+            />
+        </div>
+      );
+  }
 }
+
+export default Home;
